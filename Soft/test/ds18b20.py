18f519fcd284b4271c696accce7e4075ab4090d8
alex <alexalex78@ymail.com>
2024-04-12 15:16:10 +0300

    s

--- Soft/test/ds18b20.py
+++ Soft/test/ds18b20.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
+import time
 import board
 from adafruit_onewire.bus import OneWireBus
 from adafruit_ds18x20 import DS18X20
 
-ow_bus = OneWireBus(board.D4)
+# Initialize one-wire bus on board pin D5.
+ow_bus = OneWireBus(board.D5)
+
+# Scan for sensors and grab the first one found.
 ds18 = DS18X20(ow_bus, ow_bus.scan()[0])
-ds18.temperature
+
+# Main loop to print the temperature every second.
+while True:
+    print("Temperature: {0:0.3f}C".format(ds18.temperature))
+    time.sleep(1.0)
 
 # class DS18B20:
 #     def __init__(self, pin):
