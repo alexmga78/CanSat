2399ab7e11c06f0b08006f0f03ab1087058a0e0d
alex <alexalex78@ymail.com>
2024-04-12 15:31:26 +0300

    s

--- Soft/test/ds18b20.py
+++ Soft/test/ds18b20.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python3
+import os
+import glob
 import time
-import adafruit_onewire.bus
-import board
-import adafruit_onewire
-import adafruit_ds18x20
 
-# Initialize the OneWire bus
-ow_bus = adafruit_onewire.bus.OneWireBus(board.D4)
-
-# Initialize the DS18B20 sensor
-ds18 = adafruit_ds18x20.DS18X20(ow_bus)
-
-# Scan for DS18B20 sensors on the bus
-sensor_ids = ds18.scan()
-if sensor_ids:
-    # Set the sensor address
-    ds18.address = sensor_ids[0]
-
-    # Main loop
-    try:
-        while True:
-            # Read temperature from the sensor
-            temperature = ds18.temperature
-            print("Temperature: {:.2f} C".format(temperature))
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("Exiting...")
-else:
-    print("No DS18B20 sensor found!")
-
-
+# These tow lines mount the device:
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+# Get all the filenames begin with 28 in the path base_dir.
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+def read_rom():
+	name_file=device_folder+'/name'
+	f = open(name_file,'r')
+	return f.readline()
+ 
+def read_temp_raw():
+	f = open(device_file, 'r')
+	lines = f.readlines()
+	f.close()
+	return lines
+ 
+def read_temp():
+	lines = read_temp_raw()
+	# Analyze if the last 3 characters are 'YES'.
+	while lines[0].strip()[-3:] != 'YES':
+		time.sleep(0.2)
+		lines = read_temp_raw()
+	# Find the index of 't=' in a string.
+	equals_pos = lines[1].find('t=')
+	if equals_pos != -1:
+		# Read the temperature .
+		temp_string = lines[1][equals_pos+2:]
+		temp_c = float(temp_string) / 1000.0
+		temp_f = temp_c * 9.0 / 5.0 + 32.0
+		return temp_c, temp_f
+ 
+print(' rom: '+ read_rom())
+while True:
+	print(' C=%3.3f  F=%3.3f'% read_temp())
+	time.sleep(1)
 
 # class DS18B20:
 #     def __init__(self, pin):
